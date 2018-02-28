856b3dc792a4cbdbb30b36874a653fae0e555d8b
kamal <kamal_pandey2012@hotmail.com>
2018-02-28 12:27:24 +0530

    Edit options

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 'use strict';
-import { ExtensionContext, Disposable, commands, window, workspace, Position, Selection } from 'vscode';
+import { ExtensionContext, Disposable, commands, window, workspace, Position, Selection, Range, Uri, ShellExecution } from 'vscode';
 import * as io from 'socket.io-client';
 import { config } from './socketconfig';
 
@@ -28,19 +28,13 @@ export function activate(context: ExtensionContext) {
 
         socket.on('open_file', function (filePath: string) {
             const currentEditorPath = rootPath + filePath;
-            console.log('file open', currentEditorPath);
             workspace.openTextDocument(currentEditorPath).then(function (didOpen) {
                 window.showTextDocument(didOpen.uri).then(function (openFile) {
                     let editor = window.activeTextEditor;
                     if (!editor) {
                         return;
                     }
-                    const position = new Position(1, 4);
-                    editor.selections = [new Selection(position, position)];
-                    console.log('editor opened', openFile);
-
                 });
-                console.log('file opened', didOpen);
             });
         });
 
@@ -49,16 +43,51 @@ export function activate(context: ExtensionContext) {
             if (!editor) {
                 return;
             }
-
             const selectionData = JSON.parse(data);
             const positionAnchor = new Position(selectionData.anchor.line, selectionData.anchor.character);
             const positionActive = new Position(selectionData.active.line, selectionData.active.character);
+            const range = new Range(positionAnchor, positionActive);
+            editor.revealRange(range, 1);
             editor.selections = [new Selection(positionAnchor, positionActive)];
         });
-    });
 
-    context.subscriptions.push(disposable);
-}
+        socket.on('change_document', function (data: string) {
+            const editor = window.activeTextEditor;
+            if (!editor) {
+                return;
+            }
+            editor.edit(function (editBuilder) {
+                const editOptions = JSON.parse(data);
+                const mode = editOptions.mode;
+                const pos1 = editOptions.position1;
+                const pos2 = editOptions.position2;
+                const text = editOptions.text;
+                const position1 = new Position(pos1.line, pos1.character);
+                if (mode === 1) {
+                    editBuilder.insert(position1, text);
+                }
+                else if (mode === 2) {
+                    const position2 = new Position(pos2.line, pos2.character);
+                    const deleteRange = new Range(position1, position2);
+                    editBuilder.delete(deleteRange);
+                }
+                // editor.preserveFocus()
+
+            });
+        });
+        socket.on('file_system_change', function (data: string) {
+            const fileSystemChangeData = JSON.parse(data);
+            const changeType: string = fileSystemChangeData.action;
+            const changeUri: Uri = fileSystemChangeData.uri;
+            if (changeType === 'created') {
+                // const newShellExecution = new ShellExecution(`touch ${}`);
+            }
+        });
+
+
+
+        context.subscriptions.push(disposable);
+    }
 
 export function deactivate() {
-}
+    }
